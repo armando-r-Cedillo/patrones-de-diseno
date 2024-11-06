f36dc313f5c4ce88c7a09213417cf4d8182a1eb6
ARMANDO ROCHA CEDILLO <armando.rocha@elektra.com.mx>
2024-11-05 19:38:56 -0600

    init project

---
+++ Main.java
@@ -0,0 +1,8 @@
+import javax.xml.transform.stream.StreamSource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("abcd");
+    }
+}
---
+++ implementacion/concepto/encapsulacion/Pedido.java
@@ -0,0 +1,13 @@
+package implementacion.concepto.encapsulacion;
+
+public class Pedido {
+
+    public obtenerTotalPedido(Orden orden){
+        double total = 0.0;
+
+    }
+
+    public obtenerImpuesto(){
+
+    }
+}
